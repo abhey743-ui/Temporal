c6ef49110826f73adddc28a958fd52d6ee1336d1
abhey743-ui <abhey743@gmail.com>
2026-08-11 01:50:14 +0530

    Update Starter.java

--- Starter/Starter.java
+++ Starter/Starter.java
@@ -1,1 +1,17 @@
 
+@Component
+@AllArgsConstructor
+public class CreateTripWorkFlowStarter {
+    private WorkflowServiceStubs workflowServiceStub;
+
+    public void createTrip(MakeTripDto makeTripDto ){
+        WorkflowClient workflowClient = WorkflowClient.newInstance(workflowServiceStub);
+
+        CreateTripWorkFlowInterface createTripWorkFlowInterface = workflowClient
+                .newWorkflowStub(CreateTripWorkFlowInterface.class, WorkflowOptions
+                        .newBuilder().setWorkflowId("MAKE_TRIP"+ makeTripDto.getEmail())
+                        .setTaskQueue("CREATE_TRIP")
+                        .build()
+                );
+    }
+}
